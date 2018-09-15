7f614a685262e70a904814b8e48d5d93ceda03c5
IvanHoracio <15390233@itchetumal.edu.mx>
2018-09-14 19:06:20 -0500

    Interfaz principal terminanada. Inicio de programación

--- app/src/main/java/com/example/ivan_/actividad1/MainActivity.java
+++ app/src/main/java/com/example/ivan_/actividad1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ivan_.actividad1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void GenerarRFC(View v){
+        //Tomar la primer letra y primer vocal del apellido paterno
+        EditText Apellido_paterno = (EditText) findViewById(R.id.Apellido_pa);
+        char primerLetraAP = Apellido_paterno.getText().charAt(0);
+
+        char[] vocales = {'A','E','I','O','U', 'a','e','i','o','u'};
+        String cadAux = "";
+        Character LetraAP = new Character(cadAux.charAt())
+        //Tomar la primer letra del apellido materno;
+
+        //Tomar la primer letra del primer nombre
+
+        //Tomar los últimos 2 digitos del año del nacimiento
+
+        //Tomar los 2 digitos del mes de nacimiento
+
+        //TOmar los 2 digitos del dia de nacimiento
+
+        //Generar 3 caracteres (lestras mayúsculas o números) aleatoriamente
+
+        //Concatenar los anteriror y mostrar el rfc
+
     }
 }
